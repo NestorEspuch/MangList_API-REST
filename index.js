dd3f9da698e227a3f51b237df9a702fba1e47973
AndresMaurioDrapier <andresuqui2@gmail.com>
2023-05-07 17:11:52 +0200

    Cambio de servidor de mongo

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const comicRouter = require("./routes/comics");
 const mailRouter = require("./routes/mail");
 
 // Conectar con BD en Mongo
-mongoose.connect("mongodb+srv://andresuqui2:andresuqui2@manglist.aih5yik.mongodb.net/test", {
+//? MONGO EN MONGODB: mongodb+srv://andresuqui2:andresuqui2@manglist.aih5yik.mongodb.net/test
+//? MONGO CONTENEDOR: mongodb://mymongodb/MangList_API
+mongoose.connect("mongodb://mymongodb/MangList_API", {
     useNewUrlParser: true,
 });
 
