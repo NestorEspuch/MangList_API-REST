c5995d614ad58748d2242fea5fba292d7bd92372
AndresMaurioDrapier <andresuqui2@gmail.com>
2023-05-11 19:35:30 +0200

    comentadisimo

--- routes/auth.js
+++ routes/auth.js
@@ -43,51 +43,51 @@ router.post("/register", upload.single("avatar"), async (req, res) => {
         }
     });
 
-    let newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 8),
-        //! Cambiar cuando se solucione la subida de imagenes
-        avatar: req.file.path,
-        role: req.body.role
-    });
+    // let newUser = new User({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: bcrypt.hashSync(req.body.password, 8),
+    //     //! Cambiar cuando se solucione la subida de imagenes
+    //     avatar: req.file.path,
+    //     role: req.body.role
+    // });
 
-    User.find().then((users) => {
-        let existUser = users.filter(
-            (user) => user.name == newUser.name || user.email == newUser.email
-        );
+    // User.find().then((users) => {
+    //     let existUser = users.filter(
+    //         (user) => user.name == newUser.name || user.email == newUser.email
+    //     );
 
-        if (existUser.length > 0) {
-            res.status(400).send({
-                ok: false,
-                error: "El usuario ya existe.",
-            });
-        } else {
-            newUser
-                .save()
-                .then((result) => {
-                    if (result) {
-                        res.status(200).send({ ok: true, result: result });
-                    } else {
-                        res.status(500).send({
-                            ok: false,
-                            error: "Error al registrar el usuario.",
-                        });
-                    }
-                })
-                .catch(() => {
-                    res.status(400).send({
-                        ok: false,
-                        error: "Error al registrar el usuario: " + newUser.avatar,
-                    });
-                });
-        }
-    }).catch(() => {
-        res.status(400).send({
-            ok: false,
-            error: newUser,
-        });
-    });
+    //     if (existUser.length > 0) {
+    //         res.status(400).send({
+    //             ok: false,
+    //             error: "El usuario ya existe.",
+    //         });
+    //     } else {
+    //         newUser
+    //             .save()
+    //             .then((result) => {
+    //                 if (result) {
+    //                     res.status(200).send({ ok: true, result: result });
+    //                 } else {
+    //                     res.status(500).send({
+    //                         ok: false,
+    //                         error: "Error al registrar el usuario.",
+    //                     });
+    //                 }
+    //             })
+    //             .catch(() => {
+    //                 res.status(400).send({
+    //                     ok: false,
+    //                     error: "Error al registrar el usuario: " + newUser.avatar,
+    //                 });
+    //             });
+    //     }
+    // }).catch(() => {
+    //     res.status(400).send({
+    //         ok: false,
+    //         error: newUser,
+    //     });
+    // });
 });
 
 router.post("/login", async (req, res) => {
