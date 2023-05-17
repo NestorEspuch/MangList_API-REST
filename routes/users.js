ecc49bf2233d2ebfef37cbc4a169ee3fe440e8cb
Néstor Espuch <nestorespuch@gmail.com>
2023-05-17 20:24:30 +0200

    Servicio recuperar password - FIX

--- routes/users.js
+++ routes/users.js
@@ -121,7 +121,10 @@ router.put("/password-recovery", async (req, res) => {
                     message: "Tu nueva contraseña es: " + newPassword + " |Recuerda cambiarla nuevamente en tu perfil."
                 };
                 mail.sendMail(options).then((result) => {
-                    console.log(result);
+                    res.status(200).send({
+                        ok: true,
+                        result: result,
+                    });
                 }).catch((error) => {
                     res.status(400).send({
                         ok: false,
